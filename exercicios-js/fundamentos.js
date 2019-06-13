5e7cf51a93ee223dd2a2ae0c5fa4443eb3a19c21
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-06-13 20:06:59 -0300

    atividade 1

---
+++ exercicios-js/fundamentos.js
@@ -0,0 +1,15 @@
+// formas de executar um arquivo em javascript
+
+// jsFiddle - executar codigo javascript, css e html
+// console online
+// editores de texto, como o sublime-text e o vscode
+
+
+console.log('Teste'); //uma sentenca
+
+{//primeiro bloco de codigo
+    { // segundo bloco de codigo
+        console.log("olá"); // segunda sentenca
+        console.log("mundo!") // terceira sentenca
+    }
+}
