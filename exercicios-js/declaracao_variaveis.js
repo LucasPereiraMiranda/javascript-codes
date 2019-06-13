4d9d8b64fbcb439707985d1012a9159e4dbf7b6c
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-06-13 20:29:55 -0300

    atividade 3 - let, var e const

---
+++ exercicios-js/declaracao_variaveis.js
@@ -0,0 +1,19 @@
+
+// var
+
+var numero1 = 10;
+
+// let
+
+let numero2 = 20;
+
+// o var pode ser declarado mais de uma vez em um mesmo escopo, sem ocasionar erros
+// o let nao pode ser declarado mais de uma vez em um mesmo escopo, ocasionando erros
+
+console.log(numero1,numero2);
+
+// const
+
+const constante = 5;
+
+console.log(constante);
