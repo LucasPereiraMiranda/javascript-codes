0ae45085ae3d280a8e8e80c597fa31f6284037db
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-06-13 20:16:31 -0300

    atividade 2 - comentários

---
+++ exercicios-js/comentarios.js
@@ -0,0 +1,20 @@
+// em javascript temos algumas formas de realizar comentarios
+
+console.log("Linha 1");
+// -> duas barras
+
+console.log("Linha 2");
+
+/*
+    barra e asterisco
+*/
+
+console.log("Linha 3");
+
+//dicas quanto ao uso de comentarios:
+/*
+*evitar comentarios obvios
+*prefira codigo claro
+*usar para auxiliar no entendimento durante as aulas
+*/
+
