b724e6cb5173007c892bcdc64e46a42f06bf2589
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-06-13 23:15:11 -0300

    atividade 4 - tipagem do javascript

---
+++ exercicios-js/tipagem_fraca.js
@@ -0,0 +1,9 @@
+
+//javascript possui tipagem dinamica, ou seja, as variaveis nao tem um tipo definido ao declarar
+// a mesma. essa variavel pode mudar de tipo durante a execucao do programa
+
+let qualquer = "Legal";
+console.log(typeof(qualquer));
+
+qualquer = 3.1415;
+console.log(typeof(qualquer));
