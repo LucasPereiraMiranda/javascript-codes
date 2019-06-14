fe3b17b9f7ca9b9406d188f3db3b2d223587c109
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-06-14 20:12:37 -0300

    atividade 10 - Bollean

---
+++ exercicios-js/booleanos.js
@@ -0,0 +1,14 @@
+
+
+let isAtivo = false;
+
+console.log(`Atualmente está ativo?: ${isAtivo}`);
+
+isAtivo = true;
+
+console.log(`Atualmente está ativo?: ${isAtivo}`);
+
+isAtivo = 1;
+
+console.log(`Atualmente está ativo?: ${!!isAtivo}`); // transformando o number em uma consequencia logica
+
