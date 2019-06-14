f1495d13c11b4e546d223a5560d159ebe6b2f829
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-06-13 23:35:25 -0300

    atividade 6 - alguns cuidados com numbers

---
+++ exercicios-js/alguns_cuidados_numbers.js
@@ -0,0 +1,8 @@
+
+console.log(10/0); // javascript retorna infinity, nao levantando uma excecao
+
+console.log("5.2"/2);     
+// javascript tenta converter a string para um inteiro/float caso consiga, a operacao e realizada
+
+console.log((8.55421236).toFixed(2)); // retornando apenas 2 casas decimais
+
