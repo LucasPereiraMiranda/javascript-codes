c39b4e70c3ca3c829a45942b61baa070ac914d4a
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-07-20 16:57:55 -0300

    Entendendo a declaração de variáveis com let

---
+++ usandoLet1.js
@@ -0,0 +1,12 @@
+let numero = 1
+
+{
+    let numero = 2
+    console.log('Dentro = ', numero)
+}
+
+console.log('Fora = ',numero);
+
+// var tem escopo global e escopo de funcao
+
+// let tem escopo global, escopo de funcao e escopo de bloco
