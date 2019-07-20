34825b105b5bb2fcd9c99565eb84ae9eb604d666
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-07-20 12:18:38 -0300

    introducao a funcoes

---
+++ exercicios-js/funcaoEmQuaseTudo.js
@@ -0,0 +1,12 @@
+
+console.log(typeof(Object));
+
+// o Object em javascript e uma function
+
+class Produto {}
+
+console.log(typeof(Produto));
+
+// uma class em javascript e uma function
+
+// o javascript e altamente centrado em function
