3b714be2a7fe69e0c4c5497a4b73e573beb9a16c
Lucas Pereira Miranda <permiranda.lucas@gmail.com>
2019-07-21 17:18:41 -0300

    Atribuicao

---
+++ exercicios-js/atribuicao.js
@@ -0,0 +1,25 @@
+// atribuicao 
+const a = 7
+
+// atribuicao
+let b = 3
+
+// atribuicao de adicao, acrescentando a o valor de b a = a + b
+b += a
+console.log(b) 
+
+// atribuicao de subtracao
+b -= a
+console.log(b) 
+
+// atribuicao de divisao
+b /= a
+console.log(b) 
+
+// atribuicao de multiplicacao
+b *= a
+console.log(b) 
+
+// atribuicao de modulo
+b %= a
+console.log(b) 
